1a7c6c6231647479980a3e8dd77bc5c87a6c0eff
Ninuskaninus <ninjaa181@gmail.com>
2023-03-23 21:24:49 +0100

    Fixed popup message

--- js/contact.js
+++ js/contact.js
@@ -3,9 +3,12 @@ const sendBtn = document.querySelector(".button4");
 
 sendBtn.addEventListener("click", function(){
     const popup = document.createElement("div");
+    const popupMessage = document.createElement("p");
     popup.classList.add("popupBuy");
-    popup.innderHTML = "<p>Message sendt!</p>";
+    popup.style.backgroundColor = "green";
+    popupMessage.textContent = "Message sent!";
     contactForm.appendChild(popup);
+    popup.appendChild(popupMessage);
 
     setTimeout(function (){
         popup.remove();
